e8a2b9f78035864c3ab22ebb5a4dbab86ba1775a
shadab khan <shadabkhan2910@gmail.com>
2024-11-12 20:54:35 +0530

    route handler

--- src/app.js
+++ src/app.js
@@ -2,16 +2,50 @@ const express = require("express")
 
 const app = express()
 
-app.get("/", (req, res) => {
-    res.send("hello Shadab")
-});
+// app.get(/.*fly$/, (req, res) => {
+//     res.send("hello Shadab")
+// });
+app.use("/test", (req, res, next) => {
+    // console.log(req.params)
+    // res.send("hello Shadab")
+    console.log('handler 1')
+    next()
+},
+    [(req, res, next) => {
+        // console.log(req.params)
+        // res.send("hello Shadab")
+        console.log('handler 2')
 
-app.use("/home", (req, res) => {
-    res.send("hello from express js Home")
-});
+        next()
+    }],
+    [(req, res, next) => {
+        // console.log(req.params)
+        // res.send("hello Shadab")
+        console.log('handler 3')
 
-app.use("/test", (req, res) => {
-    res.send("hello from express js")
-});
+        next()
+    }],
+    (req, res) => {
+        // console.log(req.params)
+        console.log('handler 4')
+
+        res.send("hello Shadab 5")
+    });
+
+// app.get("/test", (req, res) => {
+//     console.log(req.query)
+//     res.send("hello Shadab")
+// });
+
+// app.post("/test", (req, res) => {
+//     console.log(req.body)
+//     res.send("data successfuly posted on the server")
+// });
+
+// app.delete("/test", (req, res) => {
+
+//     res.send("data successfuly deleted")
+// });
+
 
 app.listen(2000, () => console.log("Server is running"))
