2f4c727bf067c04c59bdbf3c84ae467fa62a9bb4
shadab khan <shadabkhan2910@gmail.com>
2025-01-09 15:13:15 +0530

    test

--- src/router/authenticate.js
+++ src/router/authenticate.js
@@ -67,7 +67,7 @@ authRouter.post("/signup", async (req, res) => {
             }
         })
         // http://localhost:5173/
-        const verificationUrl = `https://api/verify-email/${token}`
+        const verificationUrl = `https://devtinder.site/verify-email/${token}`
         // const verificationUrl = `http://localhost:2000/verify-email/${token}`
 
 
